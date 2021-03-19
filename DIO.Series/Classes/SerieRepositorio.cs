963cea060e2096373be369cefe640a2250339182
Eduardo Cupertino <37818675+educooper@users.noreply.github.com>
2021-03-19 06:30:16 -0300

    Dio.Series
    Subindo manualmente Series

---
+++ DIO.Series/Classes/SerieRepositorio.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using Dio.Series.Interfaces;
+
+namespace Dio.Series
+
+{
+    public class SerieRepositorio : iRepositorio<Serie>
+    {
+        public void Atualiza(int id, Serie endidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Exclui(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insere(Serie entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Serie> lista()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ProximoId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Serie RetornaPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
