fa383111293b5e79fd5ec9072a56e320ee48d009
Eduardo Cupertino <37818675+educooper@users.noreply.github.com>
2021-03-19 15:02:02 -0300

    Update Program.cs

--- DIO.Series/Program.cs
+++ DIO.Series/Program.cs
@@ -6,7 +6,51 @@ namespace DIO.Series
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+          string OpcaoUsuario = ObterOpcaoUsuario();
+		
+		while (OpcaoUsuario.ToUpper() != "X")
+		{
+		switch (OpcaoUsuario)
+			{
+		case "1":
+		ListarContas();
+		break;
+		case "2":
+		InserirConta();
+		break;
+		case "3":
+		Transferir();
+		break;
+		case "4":
+		Sacar();
+		break;
+		case "5":
+		Depositar();
+		break;
+		case "C":
+		Console.Clear();
+		break;
+		
+		default:
+		throw new ArgumentOutOfRangeException();
         }
+        
+        private static string ObterOpcaoUsuario()
+{
+	Console.WriteLine();
+	Console.WriteLine("DIO bank ao seu dispor!");
+	Console.WriteLine("Informe a opção Desejada:");
+	Console.WriteLine("1- Listar Contas");
+	Console.WriteLine("2- Inserir nova conta");
+	Console.WriteLine("3- Transferir");
+	Console.WriteLine("4- Sacar");
+	Console.WriteLine("5- Depositar");
+	Console.WriteLine("C- Limpar Tela");
+	Console.WriteLine("X- Sair");
+
+	string OpcaoUsuario = Console.ReadLine().ToUpper();
+	Console.WriteLine();
+	return OpcaoUsuario;
+		}
     }
 }
